39183d2e7c0ba34d5981600063c6de2b212197d0
codjoero <rownee2001@gmail.com>
2019-05-04 20:46:13 +0300

    New changes

---
+++ start/client/src/index.js
@@ -0,0 +1,45 @@
+import { Query, ApolloProvider } from 'react-apollo';
+import React from 'react';
+import gql from 'graphql-tag';
+import ReactDOM from 'react-dom';
+import Pages from './pages';
+import Login from './pages/login';
+import injectStyles from './styles';
+
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+
+const cache = new InMemoryCache();
+const link = new HttpLink({
+  uri: 'http://localhost:4000/',
+  headers: {
+    authorization: localStorage.getItem('token'),
+  },
+})
+
+const client = new ApolloClient({
+  cache,
+  link,
+})
+
+cache.writeData({
+    data: {
+        isLoggedIn: !!localStorage.getItem('token'),
+        cartItems: [],
+    }
+})
+
+const IS_LOGGED_IN =gql`
+    query IsUserLoggedIn {
+        isLoggedIn @client
+    }
+`;
+
+injectStyles();
+ReactDOM.render(
+    <ApolloProvider client={client}>
+        <Query query={IS_LOGGED_IN}>
+            {({ data }) => (data.isLoggedIn ? <Pages /> : <Login/>)}
+        </Query>
+    </ApolloProvider>, document.getElementById('root'));
---
+++ start/server/src/schema.js
@@ -0,0 +1,77 @@
+const { gql } = require('apollo-server');
+
+const typeDefs = gql`
+    type Launch {
+        id: ID!
+        site: String
+        mission: Mission
+        rocket: Rocket
+        isBooked: Boolean!
+    }
+
+    type Rocket {
+        id: ID!
+        name: String
+        type: String
+    }
+
+    type Mission {
+        name: String
+        missionPatch(size: PatchSize): String
+    }
+
+    enum PatchSize {
+        SMALL
+        LARGE
+    }
+
+    type User {
+        id: ID!
+        email: String!
+        trips: [Launch]!
+    }
+
+    type Query {
+        launches(
+            """
+            The number of results to show. Must be >= 1. Default = 20
+            """
+            pageSize: Int
+            """
+            If you add a cursor here, it will only return results _after_ this cursor
+            """
+            after: String
+        ): LaunchConnection!
+        launch(id: ID!): Launch
+        # Queries for the current user
+        me: User
+    }
+
+    """
+    Simple wrapper around our list of launches that contains a cursor to the
+    last item in the list. Pass this cursor to the launches query to fetch results
+    after these.
+    """
+    type LaunchConnection { # add this below the Query type as an additional type.
+        cursor: String!
+        hasMore: Boolean!
+        launches: [Launch]!
+    }
+
+    type Mutation {
+        # if false, booking trips failed -- check errors
+        bookTrips(launchIds: [ID]!): TripUpdateResponse!
+        # if false, cancellation failed -- check errors
+        cancelTrip(launchId: ID!): TripUpdateResponse!
+
+        login(email: String!): String #login Token
+    }
+
+    type TripUpdateResponse {
+        success: Boolean!
+        message: String
+        launches: [Launch]
+    }
+`;
+
+module.exports = typeDefs;
